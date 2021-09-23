980f110d2630e798266a93111b22bf75990f358f
Ashwani Kumar Tiwari <tiwariashwani3012@gmail.com>
2021-09-23 17:40:01 +0530

    Section 4 Completed

--- Notes_app/app.js
+++ Notes_app/app.js
@@ -61,7 +61,7 @@ yargs.command({
       type: "string",
     },
   },
-  handler: function (argv) {
+  handler(argv) {
     notes.addNote(argv.title, argv.body);
   },
 });
@@ -77,17 +77,8 @@ yargs.command({
       type: "string",
     },
   },
-  handler: function (argv) {
+  handler(argv) {
     notes.removeNote(argv.title);
-  },
-});
-
-// Create read command
-yargs.command({
-  command: "read",
-  describe: "Read a note",
-  handler: function () {
-    console.log("Reading a note");
   },
 });
 
@@ -95,8 +86,24 @@ yargs.command({
 yargs.command({
   command: "list",
   describe: "List your notes",
-  handler: function () {
-    console.log("Listing out all notes");
+  handler() {
+    notes.listNotes();
+  },
+});
+
+// Create read command
+yargs.command({
+  command: "read",
+  describe: "Read a note",
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler(argv) {
+    notes.readNote(argv.title);
   },
 });
 
--- Notes_app/notes.js
+++ Notes_app/notes.js
@@ -1,19 +1,23 @@
 const fs = require("fs");
 const chalk = require("chalk");
 
-const getNotes = function () {
+const getNotes = () => {
   return "Your notes...";
 };
 
 // add note
-const addNote = function (title, body) {
+const addNote = (title, body) => {
   const notes = loadNotes();
 
-  const duplicateNotes = notes.filter(function (note) {
-    return note.title === title;
-  });
+  // const duplicateNotes = notes.filter(function (note) {
+  //   return note.title === title;
+  // });
 
-  if (duplicateNotes.length === 0) {
+  //const duplicateNotes = notes.filter((note) => note.title === title);
+  const duplicateNote = notes.find((note) => note.title === title);
+
+  //if (duplicateNotes.length === 0) {
+  if (!duplicateNote) {
     notes.push({
       title: title,
       body: body,
@@ -27,11 +31,14 @@ const addNote = function (title, body) {
 };
 
 // remove note
-const removeNote = function (title) {
+const removeNote = (title) => {
   const notes = loadNotes();
-  const notesToKeep = notes.filter(function (note) {
-    return note.title !== title;
-  });
+
+  // const notesToKeep = notes.filter(function (note) {
+  //   return note.title !== title;
+  // });
+
+  const notesToKeep = notes.filter((note) => note.title !== title);
 
   saveNotes(notesToKeep);
 
@@ -44,12 +51,35 @@ const removeNote = function (title) {
   }
 };
 
-const saveNotes = function (notes) {
+// otes list
+const listNotes = () => {
+  const notes = loadNotes();
+  console.log(chalk.inverse("Your Notes"));
+
+  notes.forEach((note) => {
+    console.log(note.title);
+  });
+};
+
+// read note
+const readNote = (title) => {
+  const notes = loadNotes();
+  const note = notes.find((note) => note.title === title);
+
+  if (note) {
+    console.log(chalk.inverse(note.title));
+    console.log(note.body);
+  } else {
+    console.log(chalk.red.inverse("Note not found"));
+  }
+};
+
+const saveNotes = (notes) => {
   const datajson = JSON.stringify(notes);
   fs.writeFileSync("notes.json", datajson);
 };
 
-const loadNotes = function () {
+const loadNotes = () => {
   try {
     const databuffer = fs.readFileSync("notes.json");
     const dataJSON = databuffer.toString();
@@ -60,7 +90,8 @@ const loadNotes = function () {
 };
 
 module.exports = {
-  getNodes: getNotes,
   addNote: addNote,
   removeNote: removeNote,
+  listNotes: listNotes,
+  readNote: readNote,
 };
